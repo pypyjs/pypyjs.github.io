b4a3cea06628cbd7ef19f2f84f9fc8237a16a5e4
JensDiemer <git@jensdiemer.de>
2015-06-18 16:56:50 +0200

    refactor
    results here: https://gist.github.com/jedie/d650de636711aa786235

--- test_compression.py
+++ test_compression.py
@@ -1,74 +1,25 @@
-
 from __future__ import print_function
 
 import os
 import sys
 import time
-
-import six
-
+from pprint import pprint
 import zlib
-
+import bz2
 
 # New in Python 3.3: https://docs.python.org/3/library/lzma.html
-# import lzma
+import lzma
 
 
-def compress_zopfli(data):
-    """
-    https://github.com/wnyc/py-zopfli
-    https://pypi.python.org/pypi/zopfli
-    """
-    try:
-        from zopfli import zopfli
-    except ImportError as err:
-        print("Import error:\n%s" % err)
-        print("\nPyzopfli not installed?!?")
-        print("\ne.g.:")
-        print("    pip install zopfli")
-        sys.exit(-1)
-
-    return zopfli.compress(data,
-        gzip_mode=0
-    )
-
-
-def test_zopfli():
-    print("test zopfli...\n")
-
-    data = b"Hello, world."
-    print("Compress with zopfli: %r" % data)
-    compressed = compress_zopfli(data)
-
-    print("\ncompresed:", repr(compressed))
-    print()
-    print(" ".join(["%02x" % b for b in six.iterbytes(compressed)]))
-    print()
-    print("var data = new Array(%s);" % ",".join(["%i" % b for b in six.iterbytes(compressed)]))
-
-    print("\ndecompress with zlib:", zlib.decompress(compressed))
-
-
-def hello_world():
-    """
-    for copy&paste to:
-        http://nmrugg.github.io/LZMA-JS/demos/advanced_demo.html
-
-    same as:
-    $ echo "Hello, world." | zlib | hexdump -C | cut -c9-60
-      5d 00 00 80 00 ff ff ff  ff ff ff ff ff 00 24 19
-      49 98 6f 16 02 8c e8 e6  5b b1 47 f2 01 6a 44 1e
-      ff ff 8e bc 00 00
-    """
-
-    compressed = zlib.compress(bytes("Hello, world.", "ascii"), 9)
-
-    print(repr(compressed))
-    print
-    print(" ".join(["%02x" % b for b in compressed]))
-    print("var data = new Array(%s);" % ",".join(["%i" % b for b in compressed]))
-
-    print(zlib.decompress(compressed))
+try:
+    import creole
+    from creole.shared.markup_table import MarkupTable
+except ImportError as err:
+    print("Import error: %s" % err)
+    print("\nMaybe creole not installed?!?")
+    print("e.g.:")
+    print("    pip install python-creole")
+    sys.exit(-1)
 
 
 class Zlib(object):
@@ -85,108 +36,219 @@ class Zlib(object):
         return "_level%i.deflate" % self.level
 
 
-class Zopfli(object):
+class Bzip2(object):
+    def __init__(self, level=9):
+        self.level = level
+
     def get_info(self):
-        return "zopfli"
+        return "bzip2 level=%i" % self.level
 
     def compress(self, data):
-        try:
-            from zopfli import zopfli
-        except ImportError as err:
-            print("Import error:\n%s" % err)
-            print("\nPyzopfli not installed?!?")
-            print("\ne.g.:")
-            print("    pip install zopfli")
-            sys.exit(-1)
+        return bz2.compress(data, self.level)
 
-        return zopfli.compress(data,
-            gzip_mode=0, # zlib container
-            # gzip_mode=2, # gzip container
+    def filename_suffix(self):
+        return "_level%i.bz2" % self.level
 
-            verbose=True, # debugging data to stderr
 
-            numiterations = 15, # Maximum amount of times to rerun forward and
-            # backward pass to optimize LZ77 compression cost.
-            # Good values: 10, 15 for small files, 5 for files over
-            # several MB in size or it will be too slow.
-            # Default value: 15.
+class Lzma(object):
+    def __init__(self, preset=5):
+        self.preset = preset
 
-            blocksplitting = 1, # If true, splits the data in multiple deflate
-            # blocks with optimal choice for the block boundaries. Block
-            # splitting gives better compression.
-            # Default: true (1).
+    def get_info(self):
+        return "lzma preset=%i" % self.preset
 
-            blocksplittinglast = 0, # If true, chooses the optimal block
-            # split points only after doing the iterative LZ77 compression.
-            # If false, chooses the block split points first, then does
-            # iterative LZ77 on each individual block. Depending on the file,
-            # either first or last gives the best compression.
-            # Default: false (0).
-
-            blocksplittingmax = 15, #  Maximum amount of blocks to split into
-            # (0 for unlimited, but this can give extreme results that hurt
-            # compression on some files).
-            # Default value: 15.
+    def compress(self, data):
+        return lzma.compress(data,
+            format=lzma.FORMAT_ALONE, # The legacy .lzma container format.
+            check=lzma.CHECK_NONE, # No integrity check like CRC or SHA
+            preset=self.preset, # compression preset
+            filters=None, # custom filter chains
         )
 
     def filename_suffix(self):
-        return "_zopfli" #% self.level
+        return "_preset%i.lzma" % self.preset
 
 
-def compress_file(filepath, out_path, compressor):
-    out_filename="%s%s" % (
-        os.path.split(filepath)[1],
-        compressor.filename_suffix()
-    )
+class FileInfo(object):
+    def __init__(self, files):
+        self.files = files
 
-    out_filepath = os.path.join(out_path, out_filename)
-    src_name = os.path.split(filepath)[1]
-    print("\nCompress '%s' with %s to '%s'" % (
-        src_name, compressor.get_info(), out_filepath
-    ))
-
-    statinfo = os.stat(filepath)
-    uncompressed_size = statinfo.st_size
-
-    print("uncompressed.......: %6.2f MBytes" % (uncompressed_size/1024/1024))
-
-    with open(filepath, "rb") as source_file:
-        with open(out_filepath, "wb") as out_file:
-            start_time=time.time()
-            out_file.write(
-                compressor.compress(source_file.read())
-            )
-
-    duration = time.time()-start_time
-    print("compression time...: %6.2f sec." % duration)
-
-    statinfo = os.stat(out_filename)
-    compressed_size = statinfo.st_size
-    print("compressed.........: %6.2f MBytes" % (compressed_size/1024/1024))
-
-    percent = (100.0 / uncompressed_size) * compressed_size
-    print("compression ratio..: %6.2f %%" % percent)
-
-    return duration, percent
+    def print_table(self):
+        print("\n**Uncompressed sizes:**\n")
+        print("size in MB | size in Bytes  | file name")
+        print("---------- | -------------- | ---------------")
+        for file_path in self.files:
+            statinfo = os.stat(file_path)
+            uncompressed_size = statinfo.st_size
+            name = os.path.split(file_path)[1]
+            print("%7.1f MB | %8i Bytes | %s" % (
+                (uncompressed_size / 1024 / 1024),
+                uncompressed_size,
+                name,
+            ))
 
 
+class TableData(object):
+    def __init__(self):
+        super(TableData, self).__init__()
+        self._raw_data = []
+        self._used_keys = None
 
+    def append(self, **raw_data):
+        if self._used_keys is None:
+            self._used_keys = raw_data.keys()
+        else:
+            assert raw_data.keys() == self._used_keys
+        self._raw_data.append(raw_data)
+
+    def transform_file_path(self, file_path):
+        return {
+            "file name": os.path.split(file_path)[1]
+        }
+
+    def transform_compressor(self, compressor):
+        return {
+            "compressor": compressor.get_info()
+        }
+
+    def transform_compressed_size(self, compressed_size):
+        return {
+            "compressed Bytes": "%i Bytes" % compressed_size,
+            "compressed MB": "%.1f MB" % (compressed_size / 1024 / 1024),
+        }
+
+    def transform_duration(self, duration):
+        return {"duration": "%.2f sec." % duration}
+
+    def print_table(self, sort_key, reverse=False):
+        assert self._used_keys != None
+        assert sort_key in self._used_keys
+
+        # pprint(self._raw_data)
+
+        transformed_data = []
+        for raw_data in sorted(self._raw_data, key=lambda x: x[sort_key], reverse=reverse):
+            temp = {}
+            for key, value in raw_data.items():
+                trans_func = getattr(self, "transform_%s" % key, None)
+                if trans_func:
+                    temp.update(trans_func(value))
+                else:
+                    temp[key] = value
+            transformed_data.append(temp)
+
+        keys = tuple(transformed_data[0].keys())
+
+        table = MarkupTable(head_prefix="", auto_width=True)
+
+        # Hack to format in markdown syntax:
+        table.add_tr()
+        for key in keys:
+            table.add_td(key)
+
+        table.add_tr()
+        for key in keys:
+            table.add_td("-"*len(key))
+
+        for entry in transformed_data:
+            table.add_tr()
+            for key in keys:
+                table.add_td(entry[key])
+
+        content = table.get_table_markup()
+        content = content.strip()
+        print(content)
+
+
+
+class Benchmark(object):
+    def __init__(self, files, out_path, compressors):
+        self.files = files
+        self.out_path = out_path
+        self.compressors = compressors
+
+    def compress_file(self, filepath, compressor):
+        out_filename = "%s%s" % (
+            os.path.split(filepath)[1],
+            compressor.filename_suffix()
+        )
+
+        out_filepath = os.path.join(self.out_path, out_filename)
+        src_name = os.path.split(filepath)[1]
+        sys.stdout.write("| %15s | %14s" % (
+            src_name, compressor.get_info()
+        ))
+        sys.stdout.flush()
+
+        statinfo = os.stat(filepath)
+        uncompressed_size = statinfo.st_size
+
+        with open(filepath, "rb") as source_file:
+            with open(out_filepath, "wb") as out_file:
+                start_time = time.time()
+                out_file.write(
+                    compressor.compress(source_file.read())
+                )
+
+        duration = time.time() - start_time
+        statinfo = os.stat(out_filename)
+        compressed_size = statinfo.st_size
+        percent = (100.0 / uncompressed_size) * compressed_size
+
+        print(" | %6.2fsec | %6.2fMB | %6.2f%% | %s |" %(
+            duration, (compressed_size / 1024 / 1024), percent, out_filepath
+        ))
+
+        return compressed_size, duration
+
+    def run(self):
+        table_data = TableData()
+
+        print("\n\n**run compression:**\n")
+        print("| filename | compressor | duration | out size | ratio | out filename |")
+        print("| -------- | ---------- | -------- | -------- | ----- | ------------ |")
+
+        for compressor in self.compressors:
+            for file_path in self.files:
+                compressed_size, duration = self.compress_file(
+                    file_path, compressor
+                )
+                table_data.append(
+                    file_path=file_path,
+                    compressor=compressor,
+                    compressed_size=compressed_size,
+                    duration=duration,
+
+                )
+
+        print("\n")
+        return table_data
 
 
 if __name__ == "__main__":
-    # test_zopfli()
-    # hello_world()
-
-    compressors = (
-        # Zlib(level=1),
-        Zlib(level=9),
-        Zopfli(),
+    test_files = (
+        "pypyjs-release/lib/pypy.vm.js",
+        "pypyjs-release/lib/pypy.vm.js.mem"
     )
+    FileInfo(test_files).print_table()
 
-    for compressor in compressors:
-        print("="*79)
-        compress_file("pypyjs-release/lib/pypy.vm.js", "./", compressor)
-        compress_file("pypyjs-release/lib/pypy.vm.js.mem", "./", compressor)
+    compressors = []
 
-    print("="*79)
+    # compressors += [Zlib(level) for level in range(0,2)]
+    # compressors += [Bzip2(level) for level in range(1,3)]
+    # compressors += [Lzma(preset) for preset in range(0,1)]
+
+    compressors += [Zlib(level) for level in range(0,10)]
+    compressors += [Bzip2(level) for level in range(1,10)]
+    compressors += [Lzma(preset) for preset in range(0,10)]
+
+    for test_file in test_files:
+        b = Benchmark([test_file], "./", compressors)
+        table_data = b.run()
+
+        print("\n\n**Result sort by duration:**\n")
+        table_data.print_table(sort_key="duration")
+
+        print("\n\n**Result sort by compressed size:**\n")
+        table_data.print_table(sort_key="compressed_size")
 
